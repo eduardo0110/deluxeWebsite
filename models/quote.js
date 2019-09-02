4e668b4ce3658d40fd3cfd144c9bb6eb1d528b28
severeyn-eduardo <severeyneduardo@gmail.com>
2019-09-02 18:28:52 -0400

    model fixed

--- models/quote.js
+++ models/quote.js
@@ -9,12 +9,12 @@ const QuoteSchema = mongoose.Schema({
         type : String ,
         required: true,
         lowercase:true,
-        unique:true
+        sparse:true
     },
     number:{
         type: Number,
         required:true,
-        unique:true,
+        sparse:true,
         min: [0000000000, "You are missing a number"],
         max: 9999999999
 
@@ -22,7 +22,7 @@ const QuoteSchema = mongoose.Schema({
     message:{
         type:String,
         required:true,
-        unique:true
+        sparse:true
     }
     
 });
