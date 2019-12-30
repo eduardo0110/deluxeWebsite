a412064127392bb4e4e39524538d19e84900a309
severeyn-eduardo <severeyneduardo@gmail.com>
2019-12-30 14:26:43 -0500

    adad

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.render('index')});
 app.get('https://deluxesiding.com') , (req , res) => {
-    res.redirect(301 ,'https://www.deluxesiding.com/' )
+    res.redirect(301 ,'index' )
 }
 app.get('/contact', (req, res) => {
     res.render('contact')})
