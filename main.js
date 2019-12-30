661e1c2e1d3b0981abf74d71cd6f1c4065f3498a
severeyn-eduardo <severeyneduardo@gmail.com>
2019-12-30 16:05:59 -0500

    dada

--- main.js
+++ main.js
@@ -39,8 +39,17 @@ app.use(
 
 const quoteController = require('./controllers/quote-controller');
 const errorController= require('./controllers/errorController');
+function checkUrl(req, res, next) {
+    let host = req.headers.host;
+    if (!host.match(/^www\..*/i)) {
+      return res.redirect(301, "https://www." + host + req.url);
+    } else if (req.headers['x-forwarded-proto'] !== 'https') {{
+      return res.redirect('https://' + req.hostname + req.url);
+    }
+    next();
+  }}
 
-
+  app.use(checkUrl);
 
 app.get('/', (req, res) => {
     res.render('index')});
@@ -80,16 +89,8 @@ app.get('/deluxesiding.com/contact/*' , (req , res) => {
 app.get('/deluxesiding.com/*' , (req , res) => {
     res.render('index')})
 
-    function checkUrl(req, res, next) {
-        let host = req.headers.host;
-        if (!host.match(/^www\..*/i)) {
-          return res.redirect(301, "https://www." + host + req.url);
-        } else if (req.headers['x-forwarded-proto'] !== 'https') {{
-          return res.redirect('https://' + req.hostname + req.url);
-        }
-        next();
-      }}
-      app.use(checkUrl);
+   
+    
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
 
