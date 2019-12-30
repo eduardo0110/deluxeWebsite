a1545cb6ac45fea050a8abb0025343b13d5302c3
severeyn-eduardo <severeyneduardo@gmail.com>
2019-12-30 16:07:01 -0500

    dada

--- main.js
+++ main.js
@@ -39,17 +39,8 @@ app.use(
 
 const quoteController = require('./controllers/quote-controller');
 const errorController= require('./controllers/errorController');
-function checkUrl(req, res, next) {
-    let host = req.headers.host;
-    if (!host.match(/^www\..*/i)) {
-      return res.redirect(301, "https://www." + host + req.url);
-    } else if (req.headers['x-forwarded-proto'] !== 'https') {{
-      return res.redirect('https://' + req.hostname + req.url);
-    }
-    next();
-  }}
 
-  app.use(checkUrl);
+    
 
 app.get('/', (req, res) => {
     res.render('index')});
