33fc1882a2ca37096ae84aacc3b28a869a43be2b
severeyn-eduardo <severeyneduardo@gmail.com>
2019-12-30 16:47:54 -0500

    dada

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ app.get('/deluxesiding.com/*' , (req , res) => {
     res.render('index')})
 
     app.get('*',function(req,res){  
-        res.redirect(301 ,res.redirect('https://' + req.headers.host + req.url)
+        res.redirect(301 ,res.redirect('https://www.' + req.headers.host + req.url)
     )});
     app.use(checkUrl);
 app.use(errorController.pageNotFoundError);
