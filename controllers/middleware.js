4de3cf168256984404f0d1a6aa83752a75e33b35
severeyn-eduardo <severeyneduardo@gmail.com>
2019-12-31 04:28:59 -0500

    ddada

--- controllers/middleware.js
+++ controllers/middleware.js
@@ -1,13 +1,11 @@
 
-exports.checkUrl = function(req, res, next) {
-    let host = req.headers.host;
-    if (!host.match(/^www\..*/i)) {
-      return res.redirect(301, "https://www." + host + req.url);
-    } else if (req.headers['x-forwarded-proto'] !== 'https') {{
-      return res.redirect('https://' + req.hostname + req.url)
-    }
-    
+exports.checkUrl = (req, res, next) => {
+  let host = req.headers.host;
+  if (host.match(/^www\..*/i)) {
+    next();
+  } else {
+    res.redirect(301, "https://www." + host + req.url);
   }
-  next()}
+}
 
 
